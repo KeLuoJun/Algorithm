dfaa130f68af290c132ae997f915692fe727508e
Gbining <143269093+KeLuoJun@users.noreply.github.com>
2025-06-08 20:39:56 +0800

    树状数组

--- py/树状数组/HH的项链.py
+++ py/树状数组/HH的项链.py
@@ -8,23 +8,62 @@
 # 测试链接 : https://www.luogu.com.cn/problem/P1972
 
 import sys
-
-def compute(arr):
-    n = len(arr)
-    
+from typing import DefaultDict
 
 def main() -> None:
     data = list(map(int, sys.stdin.read().split()))
     ptr = 0
     n = data[ptr]
     ptr += 1
-    arr = data[ptr:ptr + n]
+    arr = [0] + data[ptr:ptr + n]
     ptr += n
     m = data[ptr]
     ptr += 1
-    for _ in range(m):
-        l, r = data[ptr], data[ptr + 1]
+    qs = []
+    for i in range(m):
+        qs.append((data[ptr], data[ptr + 1], i))
         ptr += 2
+
+    # 定义树状数组
+    tree = [0] * (n + 1)
+    # 定义树状数组的操作函数
+    def lowbit(i):
+        return i & (-i)
+
+    def add(i, v):
+        while i <= n:
+            tree[i] += v
+            i += lowbit(i)
+    
+    def query(i):
+        ans = 0
+        while i > 0:
+            ans += tree[i]
+            i -= lowbit(i)
+        return ans
+    def sum_(l, r):
+        return query(r) - query(l - 1)
+
+    # 计算过程
+    ans = [0] * m
+    mp = DefaultDict(int)  # 存放每一种颜色最右侧出现的位置
+    # 根据每个查询的右边界进行排序
+    qs_sorted = sorted(qs, key=lambda x: x[1])
+    s = 1  # 当前遍历到的最右查询位置
+    for q in range(m):
+        r = qs_sorted[q][1]
+        while s <= r:
+            color = arr[s]
+            if mp[color] != 0:
+                add(mp[color], -1)
+            add(s, 1)
+            mp[color] = s   # 更新颜色最右侧出现的位置
+            s += 1
+        l = qs_sorted[q][0]
+        i = qs_sorted[q][2]
+        ans[i] = sum_(l, r)
+    
+    print("\n".join(map(str, ans)))
 
 
 if __name__ == '__main__':
